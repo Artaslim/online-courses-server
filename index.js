9342111340da9534e3e26f66169d888db8e5dbb0
Artaslim <Taslimuddinar@gmail.com>
2022-10-27 08:49:57 +0600

    courses.json file added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("New API running");
@@ -13,6 +14,12 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find((course) => course.category_id === id);
+  res.send(selectedCourses);
 });
 
 app.listen(port, () => {
